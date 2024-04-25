70191de1c48832fd3f41c7467560f11e1b2fe681
meganw-school <wooleym@oregonstate.edu>
2024-04-25 11:02:41 -0500

    Added temporary private route to check authorization.

--- server/server.js
+++ server/server.js
@@ -2,12 +2,26 @@
 import express from 'express';
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Temporary ------------------------
+import authenticateJWT from './middleware/authenticate.js';
 app.get('/', (req, res) => {
     const name = process.env.NAME || 'World';
     res.send(`Hello ${name}!`);
 });
 
+app.get('/private', authenticateJWT, (req, res) => {
+    res.send('This is a private route');
+});
+// -----------------------------------
+
+// Map routes
+// app.use("/", indexRouter)
+// app.use("/character-sheet, characterSheetRouter)
+
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
-    console.log(`legendmama: listening on port ${port}`);
+    console.log(`legend-mama: listening on port ${port}`);
 });
