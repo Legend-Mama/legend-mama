e1635c8b1aa3a09624045f6f6bbc246dc7f756e9
meganw-school <wooleym@oregonstate.edu>
2024-04-25 12:05:39 -0500

    Reorganized temporary routes so they align with our structure.

--- server/firebase.js
+++ server/firebase.js
@@ -5,17 +5,3 @@ dotenv.config();
 firebase.initializeApp();
 export const firestore = firebase.firestore();
 export const firebaseAuth = firebase.auth();
-
-
-// let docRef = db.collection("accounts").doc("AgTsXlJKPVkHEFxNtZ7M");
-//
-// docRef.get().then((doc) => {
-//     if (doc.exists) {
-//         console.log("Document data:", doc.data());
-//     } else {
-//         // doc.data() will be undefined in this case
-//         console.log("No such document!");
-//     }
-// }).catch((error) => {
-//     console.log("Error getting document:", error);
-// });
--- server/server.js
+++ server/server.js
@@ -1,25 +1,14 @@
 // Set up according to https://cloud.google.com/run/docs/quickstarts/build-and-deploy/deploy-nodejs-service
 import express from 'express';
+import {default as tempRouter} from './routes/temp.js';
+
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
-// Temporary ------------------------
-import authenticateJWT from './middleware/authenticate.js';
-app.get('/', (req, res) => {
-    const name = process.env.NAME || 'World';
-    res.send(`Hello ${name}!`);
-});
-
-app.get('/private', authenticateJWT, (req, res) => {
-    res.send('This is a private route');
-});
-// -----------------------------------
-
 // Map routes
-// app.use("/", indexRouter)
-// app.use("/character-sheet, characterSheetRouter)
+app.use("/", tempRouter)
 
 const port = parseInt(process.env.PORT) || 8080;
 app.listen(port, () => {
